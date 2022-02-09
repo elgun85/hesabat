2134f5678f117ee428f012cf87efe8dc974a8f51
elgun <elgun.huseynov@azeurotel.az>
2022-02-09 14:50:40 +0400

    2

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -30,6 +30,9 @@
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contactsdfsdfd</a>
+        <a href="#" class="nav-link">Contactsdfsdfd</a>
+        <a href="#" class="nav-link">Contactsdfsdfd</a>
+        <a href="#" class="nav-link">Contactsdfsdfd</a>
 
 
 
